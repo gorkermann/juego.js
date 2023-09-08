9661006a1cc30e522c4255e877ce41abf9ee8cc9
graham <lolgorithm@gmail.com>
2023-09-07 17:07:34 -0700

    added hue and lum skewing

--- Material.ts
+++ Material.ts
@@ -1,7 +1,10 @@
 export class Material {
-	hue: number = 0; // 0-360
-	sat: number = 0; // 0.0-1.0
-	lum: number = 0; // 0.0-1.0
+	hue: number = 0.0; // 0-360
+	sat: number = 0.0; // 0.0-1.0
+	lum: number = 0.0; // 0.0-1.0
+
+	skewH: number = 0.0;
+	skewL: number = 0.0; // -1.0-1.0
 
 	constructor( hue: number, sat: number, lum: number ) {
 		this.hue = hue;
@@ -10,10 +13,20 @@ export class Material {
 	}
 
 	copy(): Material {
-		return new Material( this.hue, this.sat, this.lum );
+		let mat = new Material( this.hue, this.sat, this.lum );
+		mat.skewH = this.skewH;
+		mat.skewL = this.skewL;
+
+		return mat;
 	}
 
 	getFillStyle(): string {
-		return 'hsl(' + this.hue + ',' + this.sat * 100 + '%,' + this.lum * 100 + '%)';
+		let h = ( this.hue + this.skewH ) % 360;
+
+		let l = this.lum + this.skewL;
+		if ( l < 0.0 ) l = 0.0;
+		if ( l > 1.0 ) l = 1.0
+
+		return 'hsl(' + h + ',' + this.sat * 100 + '%,' + l * 100 + '%)';
 	}
 }
